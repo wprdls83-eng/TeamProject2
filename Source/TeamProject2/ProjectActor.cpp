eae8ed2e80253fa3de813de5fcb507db02519fdd
LAPTOP-LDBOS3C0\wprdl <wprdls83@gmail.com>
2026-03-27 20:45:29 +0900

    [Other] 새로운 actor 파일 추가

---
+++ Source/TeamProject2/ProjectActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectActor.h"
+
+// Sets default values
+AProjectActor::AProjectActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AProjectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
